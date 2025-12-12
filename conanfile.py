6fe1a15bfa660cc2952df542fee9e0ea0c6c4873
Jonas Kabelitz <jonas.kabelitz@belden.com>
2025-12-12 14:56:34 +0100

    fixed some cmake stuff

--- conanfile.py
+++ conanfile.py
@@ -1,6 +1,7 @@
 import os
 from conan import ConanFile
 from conan.tools.cmake import CMakeDeps, CMakeToolchain, CMake, cmake_layout
+from conan.tools.system.package_manager import Apt
 
 class libsbx_recipe(ConanFile):
   name = "libsbx"
@@ -38,29 +39,33 @@ class libsbx_recipe(ConanFile):
   # Source directories
   exports_sources = (
     "CMakeLists.txt",
-    "libsbx-units/**",
-    "libsbx-utility/**",
-    "libsbx-async/**",
-    "libsbx-audio/**",
+    "cmake/modules/**",
+    "scripts/**",
+    "libsbx-animations/**",
     "libsbx-bitmaps/**",
-    "libsbx-io/**",
-    "libsbx-math/**",
-    "libsbx-memory/**",
     "libsbx-core/**",
-    "libsbx-devices/**",
-    "libsbx-graphics/**",
     "libsbx-ecs/**",
+    "libsbx-gizmos/**",
+    "libsbx-io/**",
+    "libsbx-memory/**",
+    "libsbx-physics/**",
     "libsbx-scenes/**",
+    "libsbx-shadows/**",
+    "libsbx-ui/**",
+    "libsbx-utility/**",
+    "libsbx-assets/**",
+    "libsbx-containers/**",
+    "libsbx-devices/**",
+    "libsbx-editor/**",
+    "libsbx-graphics/**",
+    "libsbx-math/**",
+    "libsbx-models/**",
+    "libsbx-post/**",
     "libsbx-scripting/**",
     "libsbx-signals/**",
-    "libsbx-assets/**",
-    "libsbx-models/**",
-    "libsbx-ui/**",
-    "libsbx-shadows/**",
-    "libsbx-physics/**",
+    "libsbx-units/**",
     "demo/**",
-    "!demo/assets/**",
-    "scripts/**"
+    # "!demo/assets/**"
   )
 
   def config_options(self):
@@ -81,10 +86,25 @@ class libsbx_recipe(ConanFile):
 
     self.folders.generators = os.path.join(self.folders.build, "dependencies")
 
+    self.cpp.source.components["utility"].includedirs = ["libsbx-utility"]
+    self.cpp.source.components["core"].includedirs = ["libsbx-core"]
+    self.cpp.source.components["editor"].includedirs = ["libsbx-editor"]
+
+    self.cpp.build.components["core"].libdirs = [self.folders.build]
+    self.cpp.build.components["utility"].libdirs = [self.folders.build]
+    self.cpp.build.components["editor"].libdirs = [self.folders.build]
+
+  def system_requirements(self):
+    apt = Apt(self)
+    apt.install(["dotnet-sdk-9.0"], update=True, check=True)
+
+  def build_requirements(self):
+    self.tool_requires("cmake/[>=3.20]")
+
   def requirements(self):
-    self.requires("fmt/11.2.0")
-    self.requires("spdlog/1.15.3")
-    self.requires("glm/1.0.1")
+    self.requires("fmt/11.2.0", transitive_headers=True)
+    self.requires("spdlog/1.15.3", transitive_headers=True)
+    # self.requires("glm/1.0.1")
     self.requires("yaml-cpp/0.7.0")
     self.requires("nlohmann_json/3.11.3")
     self.requires("base64/0.5.2")
@@ -96,23 +116,24 @@ class libsbx_recipe(ConanFile):
     # self.requires("spirv-tools/1.4.309.0")
     # self.requires("slang/0.9")
     self.requires("vulkan-memory-allocator/3.3.0")
+    self.requires("vulkan-headers/1.4.321.0")
     self.requires("stb/cci.20230920")
-    self.requires("range-v3/0.12.0")
+    self.requires("range-v3/0.12.0", transitive_headers=True)
     self.requires("freetype/2.14.1")
     self.requires("gtest/1.17.0")
     # self.requires("openal-soft/1.22.2")
-    self.requires("drwav/0.13.12")
-    self.requires("drmp3/0.6.34")
+    # self.requires("drwav/0.13.12")
+    # self.requires("drmp3/0.6.34")
     self.requires("imgui/1.91.8-docking")
     self.requires("implot/0.16-docking")
     self.requires("imnodes/0.5.0-docking")
     # self.requires("imguizmo/1.83-docking")
-    self.requires("portable-file-dialogs/0.1.0")
+    # self.requires("portable-file-dialogs/0.1.0")
     self.requires("easy_profiler/2.1.0")
     self.requires("tsl-robin-map/1.3.0")
     self.requires("lz4/1.10.0")
     self.requires("assimp/5.4.3")
-    self.requires("bullet3/3.25")
+    # self.requires("bullet3/3.25")
     self.requires("meshoptimizer/0.25")
     self.requires("sol2/3.5.0")
     self.requires("magic_enum/0.9.7")
@@ -141,5 +162,76 @@ class libsbx_recipe(ConanFile):
     cmake.install()
 
   def package_info(self):
-    self.cpp_info.components["core"].libs = ["core"]
-    self.cpp_info.components["core"].includedirs = ["include"]
+    self.cpp_info.set_property("cmake_file_name", "libsbx")
+    self.cpp_info.set_property("cmake_target_name", "libsbx::libsbx")
+    
+    utility = self.cpp_info.components["utility"]
+    utility.libs = ["utility"]
+    utility.requires = [
+      "fmt::fmt", 
+      "spdlog::spdlog",
+      "magic_enum::magic_enum",
+      "lz4::lz4"
+    ]
+
+    math = self.cpp_info.components["math"]
+    math.libs = ["math"]
+    math.requires = [
+      "fmt::fmt",
+      "yaml-cpp::yaml-cpp",
+      "range-v3::range-v3",
+      "gtest::gtest"
+    ]
+
+    core = self.cpp_info.components["core"]
+    core.libs = ["core"]
+    core.requires = [
+      "range-v3::range-v3",
+      "easy_profiler::easy_profiler"
+    ]
+
+    devices = self.cpp_info.components["devices"]
+    devices.libs = ["devices"]
+    devices.requires = [
+      "glfw::glfw"
+    ]
+
+    graphics = self.cpp_info.components["graphics"]
+    graphics.libs = ["graphics"]
+    graphics.requires = [
+      "vulkan-memory-allocator::vulkan-memory-allocator",
+      "vulkan-headers::vulkan-headers",
+      "stb::stb",
+      "spirv-cross::spirv-cross"
+    ]
+
+    models = self.cpp_info.components["models"]
+    models.libs = ["models"]
+    models.requires = [
+      "tinyobjloader::tinyobjloader",
+      "nlohmann_json::nlohmann_json",
+      "base64::base64",
+      "tsl-robin-map::tsl-robin-map",
+      "assimp::assimp",
+      "meshoptimizer::meshoptimizer"
+    ]
+
+    scripting = self.cpp_info.components["scripting"]
+    scripting.libs = ["scripting"]
+    scripting.requires = [
+      "sol2::sol2"
+    ]
+
+    ui = self.cpp_info.components["ui"]
+    ui.libs = ["ui"]
+    ui.requires = [
+      "freetype::freetype"
+    ]
+
+    editor = self.cpp_info.components["editor"]
+    editor.libs = ["editor"]
+    editor.requires = [
+      "imgui::imgui",
+      "implot::implot",
+      "imnodes::imnodes"
+    ]

38c02e6d87264aeb653da640850535134211044c
KabelitzJ <jonas-kabelitz@gmx.de>
2026-02-17 18:20:09 +0100

    fdgdfg

--- libsbx-memory/libsbx/memory/tracking_allocator.cpp
+++ libsbx-memory/libsbx/memory/tracking_allocator.cpp
@@ -5,12 +5,13 @@ namespace sbx::memory {
 
 thread_local allocation_scope::context allocation_scope::_current{allocation_category::unknown, "unknown", 0u};
 
-} // namespace sbx::memory
-
 inline thread_local auto is_inside_allocation = false;
 
+} // namespace sbx::memory
+
+
 auto operator new(std::size_t size) -> void* {
-  if (!sbx::memory::is_memory_tracking_enabled_v || is_inside_allocation) {
+  if (!sbx::memory::is_memory_tracking_enabled_v || sbx::memory::is_inside_allocation) {
     if (auto* ptr = std::malloc(size)) {
       return ptr;
     }
@@ -18,18 +19,18 @@ auto operator new(std::size_t size) -> void* {
     throw std::bad_alloc{};
   }
 
-  is_inside_allocation = true;
+  sbx::memory::is_inside_allocation = true;
 
   const auto& context = sbx::memory::allocation_scope::current();
 
   try {
     auto* ptr = sbx::memory::detail::aligned_allocate( size, alignof(std::max_align_t), context.category, context.file_name, context.line);
 
-    is_inside_allocation = false;
+    sbx::memory::is_inside_allocation = false;
 
     return ptr;
   } catch (...) {
-    is_inside_allocation = false;
+    sbx::memory::is_inside_allocation = false;
 
     throw;
   }
@@ -49,17 +50,17 @@ auto operator delete(void* ptr) noexcept -> void {
     return;
   }
 
-  if (!sbx::memory::is_memory_tracking_enabled_v || is_inside_allocation) {
+  if (!sbx::memory::is_memory_tracking_enabled_v || sbx::memory::is_inside_allocation) {
     std::free(ptr);
 
     return;
   }
 
-  is_inside_allocation = true;
+  sbx::memory::is_inside_allocation = true;
 
   sbx::memory::detail::aligned_deallocate(ptr);
 
-  is_inside_allocation = false;
+  sbx::memory::is_inside_allocation = false;
 }
 
 auto operator delete[](void* ptr) noexcept -> void {

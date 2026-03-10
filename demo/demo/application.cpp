0d9f79be8690471a6f4bd0a0e7e1c534eb0fe653
Jonas Kabelitz <jonas.kabelitz@belden.com>
2026-03-10 18:09:04 +0100

    sadfsadf

--- demo/demo/application.cpp
+++ demo/demo/application.cpp
@@ -133,7 +133,7 @@ application::application()
   tree_trunk_material.metallic_factor = 0.0f;
   tree_trunk_material.roughness_factor = 0.8f;
   tree_trunk_material.sway_speed = 0.8f;
-  tree_trunk_material.sway_strength = 0.3f;
+  tree_trunk_material.sway_strength = 0.04f;
   tree_trunk_material.sway_falloff_exponent = 3.0f;
 
   auto& tree_branch_material = scene.add_material<sbx::models::material>("tree_branch");
@@ -144,10 +144,10 @@ application::application()
   tree_branch_material.roughness_factor = 0.8f;
   tree_branch_material.specular_factor = 0.25f;
   tree_branch_material.sway_speed = 1.0f;
-  tree_branch_material.sway_strength = 0.4f;
+  tree_branch_material.sway_strength = 0.06f;
   tree_branch_material.sway_falloff_exponent = 2.0f;
   tree_branch_material.scrumble_speed = 3.0f;
-  tree_branch_material.scrumble_strength = 0.15f;
+  tree_branch_material.scrumble_strength = 0.02f;
   tree_branch_material.scrumble_falloff_exponent = 1.5f;
 
   auto tree_submeshes = std::vector<sbx::scenes::static_mesh::submesh>{

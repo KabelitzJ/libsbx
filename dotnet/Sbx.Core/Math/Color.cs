2a0ef89730c1cb1b62cd06dd9ecb79be93d59ee3
KabelitzJ <jonas-kabelitz@gmx.de>
2026-09-19 21:51:34 +0200

    added lerp to color

--- dotnet/Sbx.Core/Math/Color.cs
+++ dotnet/Sbx.Core/Math/Color.cs
@@ -29,5 +29,12 @@ namespace Sbx.Core.Math
       return $"Color[R={R}, G={G}, B={B}, A={A}]";
     }
 
+    public static Color Lerp(Color a, Color b, float t)
+    {
+      t = t < 0.0f ? 0.0f : (t > 1.0f ? 1.0f : t);
+
+      return new Color(a.R + (b.R - a.R) * t, a.G + (b.G - a.G) * t, a.B + (b.B - a.B) * t, a.A + (b.A - a.A) * t);
+    }
+
   } // struct Color
 }
